19a80510b3e77dbef3b0d4f9305123bf54b89037
segin-GH <segin.bytes@gmail.com>
2023-01-17 00:19:19 +0530

    clean up

--- cerialWriter.py
+++ cerialWriter.py
@@ -1,29 +1,5 @@
 #! /usr/bin/python3
 
-# import serial
-
-# if __name__ == "__main__":
-#     ser = serial.Serial(
-#         port='/dev/ttyUSB0',
-#         baudrate=115200,
-#         # bytesize=serial.EIGHTBITS,
-#         # parity=serial.PARITY_NONE,
-#         # stopbits=serial.STOPBITS_ONE,
-#         # xonxoff=False,
-#         # rtscts=False,
-#         # dsrdtr=False
-#     )
-
-#     ser.isOpen()
-
-#     while True:
-#         input_data = input("Enter data to send (type 'exit' to exit): ")
-#         if input_data == 'exit':
-#             ser.close()
-#             break
-#         ser.write(input_data.encode())
-
-
 import serial
 
 def main():

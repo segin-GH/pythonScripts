4ed016830f9bd96b8141437147c577a181f234b9
segin-GH <segin.bytes@gmail.com>
2023-02-14 19:28:14 +0530

    .

--- CRUD_json/main.py
+++ CRUD_json/main.py
@@ -2,26 +2,46 @@ import json
 
 # Storing data in a list of dictionaries
 
-data = {
+# data = {
+#     "MSTR": {
+#         "isConnected": False,
+#         "DEV1": "PDB"
+#     },
 
-    "MSTR": {
-        "isConnected": False,
-        "DEV1": "PDB"
-    },
+#     "SLV1": {
+#         "isConnected": False,
+#         "DEV1": "CAS1",
+#         "DEV2": "CAS2"
+#     },
 
-    "SLV1": {
-        "isConnected": False,
-        "DEV1": "CAS1",
-        "DEV2": "CAS2"
-    },
+#     "SLV2": {
+#         "isConnected": False,
+#         "DEV1": "CAS3",
+#         "DEV2": "CAS4"
+#     }
+# }
 
-    "SLV2": {
-        "isConnected": False,
-        "DEV1": "CAS3",
-        "DEV2": "CAS4"
-    }
-}
+# # Writing the data to a JSON file with indentation
+# with open("data.json", "w") as f:
+#     json.dump(data, f, indent=2)
 
-# Writing the data to a JSON file with indentation
-with open("data.json", "w") as f:
-    json.dump(data, f, indent=2)
+
+# with open('data.json', 'r') as f:
+#     data = json.load(f)
+
+# mstr = data["MSTR"]
+# slv1 = data["SLV1"]
+# slv2 = data["SLV2"]
+
+# Printing the values
+# print(mstr)
+# print(slv1)
+# print(slv2)
+
+with open('data.json','r') as f:
+    data = json.load(f)
+
+data["MSTR"]["isConnected"] = True
+
+with open("curr-data.json", 'w') as f:
+    json.dump(data, f, indent=2) 

946fd4658a5c15c1b807330507d9b8fcf193091b
segin-GH <segin.bytes@gmail.com>
2023-07-20 16:30:42 +0530

    this file prints the csv in cool format

---
+++ csvTabulatePrint.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+
+import csv
+import os
+import tabulate
+import sys
+
+
+def print_logfile_tabulate(file):
+    """function to print log file csv"""
+    with open(file, "r") as f:
+        csv_reader = csv.reader(f)
+        rows = list(csv_reader)
+        print(tabulate(rows, tablefmt="pipe"))
+        
+if __name__ == "__main__":
+    # get the file need to be printed as args
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
+        print_logfile_tabulate(file)

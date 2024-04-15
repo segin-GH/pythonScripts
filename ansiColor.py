b6158ab5a3fa60d135ee3c1160f2164ba1353f7a
segin-GH <segin.bytes@gmail.com>
2024-04-15 13:23:32 +0530

    update ansi prints

--- ansiColor.py
+++ ansiColor.py
@@ -1,10 +1,15 @@
 #! /usr/bin/python3
 
-def print_256_colors():
+
+def print_256_colors_for_c():
     for i in range(0, 256):
-        print(f"\033[38;5;{i}m {i:3} \033[0m", end="")
+        # Constructing the escape sequence for C with double backslashes
+        c_escape_sequence = f"\\033[38;5;{i}m"
+        # Displaying the color, its number, and the C escape sequence
+        print(f"\033[38;5;{i}m {i:3} {c_escape_sequence} \033[0m")
         if (i + 1) % 16 == 0:
             print()
+
 
 def print_true_colors():
     step = 51  # Adjust for fewer/more steps
@@ -14,7 +19,17 @@ def print_true_colors():
                 print(f"\033[38;2;{r};{g};{b}m True Color: ({r},{g},{b}) \033[0m")
 
 
-if __name__=="__main__":
-    #print_256_colors()
-     print_true_colors()
+# print the colors with ansi code
+def print_ansi_colors():
+    for i in range(0, 8):
+        print(f"\033[3{i}m {i} \033[0m", end="")
+    print()
+    for i in range(0, 8):
+        print(f"\033[4{i}m {i} \033[0m", end="")
+    print()
 
+
+if __name__ == "__main__":
+    print_256_colors_for_c()
+    #  print_true_colors()
+    #  print_ansi_colors()

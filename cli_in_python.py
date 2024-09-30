2e1d88438ed680f82308f093c4735d307ceca0b0
segin-GH <segin.bytes@gmail.com>
2024-09-30 10:59:39 +0530

    example to cli in python

--- cli_in_python.py
+++ cli_in_python.py
@@ -1,7 +1,18 @@
 import subprocess
 
 # Running a simple CLI command, for example, 'ls' (Linux/macOS) or 'dir' (Windows)
-command = ["ls"]  # Change to 'dir' on Windows
+command = ["mcumgr", "version"]
+
+# command = [
+#     "mcumgr",
+#     "conn",
+#     "add",
+#     "testDK",
+#     "type=serial",
+#     "connstring=dev=/dev/ttyUSB1,baud=115200,mtu=512",
+# ]
+#
+command = ["mcumgr", "-c", "usb1", "image", "list"]
 
 # Run the command and capture the output
 result = subprocess.run(
@@ -11,6 +22,7 @@ result = subprocess.run(
 # Print the command's output
 print("Output:")
 print(result.stdout)
+
 
 # Print any error messages
 if result.stderr:
